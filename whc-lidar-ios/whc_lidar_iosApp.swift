77a59095f2e06f8832ecfffc21f72d1f9f913956
MEVERIK <matejkocanda@icloud.com>
2026-02-16 18:06:50 +0100

    init: RoomPlan iOS app project

---
+++ whc-lidar-ios/whc_lidar_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  whc_lidar_iosApp.swift
+//  whc-lidar-ios
+//
+//  Created by Matěj Kocanda on 16.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct whc_lidar_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
